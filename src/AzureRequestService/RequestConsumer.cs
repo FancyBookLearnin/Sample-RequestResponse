9d3f49945bf75ef82d6165b2fb13cff5c86286f5
ChocolateMonkey <monkeyengineer@outlook.com>
2016-06-07 23:24:37 +0100

    Azure Request Service

---
+++ src/AzureRequestService/RequestConsumer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AzureRequestService
+{
+    using System.Threading.Tasks;
+    using MassTransit;
+    using MassTransit.Logging;
+    using Sample.MessageTypes;
+
+
+    public class RequestConsumer :
+        IConsumer<ISimpleRequest>
+    {
+        readonly ILog _log = Logger.Get<RequestConsumer>();
+
+        public async Task Consume(ConsumeContext<ISimpleRequest> context)
+        {
+            await Console.Out.WriteLineAsync("hello");
+            _log.InfoFormat("Returning name for {0}", context.Message.CustomerId);
+
+            context.Respond(new SimpleResponse
+            {
+                CusomerName = string.Format("Customer Number {0}", context.Message.CustomerId)
+            });
+        }
+
+
+        class SimpleResponse :
+            ISimpleResponse
+        {
+            public string CusomerName { get; set; }
+        }
+    }
+}
---
+++ src/AzureRequestService/RequestService.cs
@@ -0,0 +1,52 @@
+﻿namespace AzureRequestService
+{
+    using System;
+    using System.Configuration;
+    using MassTransit;
+    using MassTransit.AzureServiceBusTransport;
+    using Topshelf;
+    using Topshelf.Logging;
+    using Microsoft.ServiceBus;
+
+    class RequestService :
+        ServiceControl
+    {
+        readonly LogWriter _log = HostLogger.Get<RequestService>();
+
+        IBusControl _busControl;
+
+        public bool Start(HostControl hostControl)
+        {
+            _log.Info("Creating bus...");
+            _busControl = Bus.Factory.CreateUsingAzureServiceBus(cfg =>
+            {
+                cfg.Host(new Uri(ConfigurationManager.AppSettings["EndPoint"]),
+                    h =>
+                    {
+                        h.TokenProvider =
+                             TokenProvider.CreateSharedAccessSignatureTokenProvider(
+                                 ConfigurationManager.AppSettings["SharedAccessKeyName"],
+                                 ConfigurationManager.AppSettings["SharedAccessKey"]);
+                    });
+                
+                cfg.ReceiveEndpoint("moviemagic", e => { e.Consumer<RequestConsumer>(); });
+            });
+           
+
+            _log.Info("Starting bus...");
+
+            _busControl.Start();           
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            _log.Info("Stopping bus...");
+
+            _busControl?.Stop();
+
+            return true;
+        }
+    }
+}
